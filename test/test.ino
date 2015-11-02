1ed01c33bdc5ca41d9c80b2dac4dd0a0ff4b271a
MedAmini <moamal@TY1412164.utu.fi>
2015-11-02 11:21:51 +0200

    test modified

--- test/test.ino
+++ test/test.ino
@@ -4,28 +4,19 @@ int Stop = 7;
 void setup(){
   pinMode(Start,OUTPUT);
   pinMode(Stop,OUTPUT);
-         digitalWrite(Start, LOW);
-          digitalWrite(Stop, LOW);
+  digitalWrite(Start, LOW);
+  digitalWrite(Stop, LOW);
 }
-
-
 
 void loop(){
   
-      //start the Timer
-      digitalWrite(Start, HIGH);// 
-      delayMicroseconds(1);
-      //for(int i =0 ; i< 50; i++);
-      digitalWrite(Start, LOW);// 
-      delayMicroseconds(1);// the time needed to count.........................
-      digitalWrite(Stop, HIGH);
-      delayMicroseconds(1);
-      //for(int i =0 ; i< 50; i++);
-      digitalWrite(Stop, LOW);
-      //delay(5000);
-
-
-//digitalWrite(Stop, LOW);
-//digitalWrite(Start, LOW);
+    digitalWrite(Start, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(Start, LOW); 
+    delayMicroseconds(1);// the time needed to count.........................
+    digitalWrite(Stop, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(Stop, LOW);
+    delay(1000);
     
 }
